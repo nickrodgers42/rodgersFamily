79af594d05021b807ac8b17ca6629716a20ee29d
Nicholas Rodgers <nick.rodgers42@gmail.com>
2018-05-28 23:08:24 +0000

    I messed something up.

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import psycopg2
+import json
 
 def index(request):
-    context={}
+    conn = psycopg2.connect("dbname=rodgersfamily user=nrodgers password=lazeratz")
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT section FROM recipes ORDER BY section ASC;")
+    subSections = cur.fetchall() 
+    context={
+    }
     return render(request, 'recipes/index.html', context) 
 
+def getSubsections(request):
+    if 'section' in request.GET:
+        section = request.GET['section']
+    conn = psycopg2.connect("dbname=rodgersfamily user=postgres password=postgres")
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT section FROM recipes ORDER BY section ASC;")
+    subSections = cur.fetchall()
+    resp = {
+        'subSections': subSections
+    }
+    return HttpResponse(json.dumps(resp))
+
 def recipe(request, recipe_id):
-    context={}
+    conn = psycopg2.connect("dbname=rodgersfamily user=nrodgers password=lazercatz")
+    cur = conn.cursor()
+    cur.execute("SELECT DISTINCT section FROM recipes ORDER BY section ASC;")
+    subSections = cur.fetchall() 
+    context={
+        'subSections': subSections
+    }
     return render(request, 'recipes/recipe.html', context)
